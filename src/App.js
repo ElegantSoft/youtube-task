3f2a4328ed48fdb6d68d16def1ab4e9432613434
ElegantSoft <react.laravel.dev@gmail.com>
2020-01-23 04:02:14 +0200

    add content loading placeholder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+// import NProgress from "nprogress";
 import Header from "./components/Header";
+import VideoPlaceholder from "./components/VideoPlaceholder";
+
+// NProgress.start();
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  /**
+   * render loader before get content
+   */
+  const renderContent = () => {
+    if (!loaded) {
+      return (
+        <div className="loader">
+          <VideoPlaceholder />;
+        </div>
+      );
+    }
+    return (
+      <div className="container">
+        <h1>content</h1>
+      </div>
+    );
+  };
+
   return (
     <>
       <div className="body">
         <Header />
-        <div className="container">
-          <h1>test</h1>
-        </div>
+        {renderContent()}
       </div>
     </>
   );
---
+++ src/components/VideoPlaceholder/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+function VideoPlaceholder() {
+  return (
+    <ContentLoader
+      width={400}
+      height={800}
+      speed={2}
+      ariaLabel="Loading news..."
+    >
+      <rect x="42.84" y="10" rx="5" ry="5" width="143.55" height="86.59" />
+      <rect x="192.84" y="30" rx="0" ry="0" width="148.72" height="12.12" />
+      <rect x="192.84" y="50" rx="0" ry="0" width="89" height="9" />
+
+      <rect x="42.84" y="110" rx="5" ry="5" width="143.55" height="86.59" />
+      <rect x="192.84" y="130" rx="0" ry="0" width="148.72" height="12.12" />
+      <rect x="192.84" y="150" rx="0" ry="0" width="89" height="9" />
+
+      <rect x="42.84" y="210" rx="5" ry="5" width="143.55" height="86.59" />
+      <rect x="192.84" y="230" rx="0" ry="0" width="148.72" height="12.12" />
+      <rect x="192.84" y="250" rx="0" ry="0" width="89" height="9" />
+      <rect x="42.84" y="310" rx="5" ry="5" width="143.55" height="86.59" />
+      <rect x="192.84" y="330" rx="0" ry="0" width="148.72" height="12.12" />
+      <rect x="192.84" y="350" rx="0" ry="0" width="89" height="9" />
+    </ContentLoader>
+  );
+}
+
+export default VideoPlaceholder;
